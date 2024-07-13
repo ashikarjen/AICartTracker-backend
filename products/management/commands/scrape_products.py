6ca5b9d87ce0a22d75132b1101ad79de8f46ec7d
Md. Ashikur Rahman <ashikarjen1234@gmail.com>
2024-07-13 11:14:04 +0600

    updated initial scraper functionality

--- products/management/commands/scrape_products.py
+++ products/management/commands/scrape_products.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from products.scraper import scrape_products
+from products.daraz import DarazScraper
 
 class Command(BaseCommand):
-    help = 'Scrape products from the website and store them in the database'
+    help = 'Scrapes products from Daraz and saves them to an Excel file'
 
     def handle(self, *args, **kwargs):
-        scrape_products()
-        self.stdout.write(self.style.SUCCESS('Successfully scraped products'))
+        scraper = DarazScraper()
+        scraper.scrape_products()
